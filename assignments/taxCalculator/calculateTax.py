6e5633f540903573be130a9ff37a7807f1f161c8
FIRST_NAME LAST_NAME <nazreen.peerzade@gmail.com>
2020-01-25 17:09:28 +0530

    Nazreen automation

--- assignments/taxCalculator/calculateTax.py
+++ assignments/taxCalculator/calculateTax.py
@@ -3,20 +3,20 @@ def netIncomeCalculator(state, grossIncome):
     statetax=0
     netIncome=0
     federalTax=10*grossIncome/100
-    if state is "Florida":
+    if state == "Florida":
         statetax=3*grossIncome/100
-    elif state is "Texas":
+    elif state == "Texas":
         statetax=5*grossIncome/100
-    elif state is "Arizona":
+    elif state == "Arizona":
         statetax=7*grossIncome/100
-    #elif state is "NewYork":
-     #   statetax=9*grossIncome/100
+    elif state is "NewYork":
+       statetax=9*grossIncome/100
     else:
-       # print("No state tax")
+       print("No state tax")
         statetax=9*grossIncome/100
     netIncome=grossIncome-federalTax-statetax
     print("netIncome for state is " , str(netIncome))
 
-statename= input("Enter state name")
-income= input("Enter grossIncome")
+#statename= input("Enter state name")
+#income= input("Enter grossIncome")
 z = netIncomeCalculator(statename,income)
